ee02f2edb96eef944c35aaa7152e0d0634e23ed4
Simarjeet Singh <simarjeetss529@gmail.com>
2023-08-23 03:21:08 +0530

    leetcode problem no :- 217 https://leetcode.com/problems/contains-duplicate/description/

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,18 +1,20 @@
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class ContainsDuplicate {
     public static void main(String[] args) {
-        int[] arr = {1,1,2,4};
+        int[] arr = {1,2,4};
         boolean ans = containsDuplicate(arr);
         System.out.println(ans);
     }
     public static boolean containsDuplicate(int[] nums){
-        Arrays.sort(nums);
+        HashSet<Integer> set = new HashSet<Integer>();
 
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i] == nums[i+1]){
+        for(int i : nums){
+            if(set.contains(i)){
                 return true;
             }
+            set.add(i);
         }
         return false;
     }
